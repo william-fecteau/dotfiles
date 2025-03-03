dd293dbbd91757869ff9958859b4472067432eab
William Fecteau <william-fecteau@hotmail.com>
2025-03-03 15:31:53 -0500

    Added lazygit

--- system-setup.sh
+++ system-setup.sh
@@ -60,4 +60,16 @@ else
 	echo "Pyenv already installed"
 fi
 
+
+echo "==============================================Installing lazygit============================================="
+if command -v lazygit &> /dev/null; then
+	echo "Lazygit is already installed"
+else
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit -D -t /usr/local/bin/
+	rm lazygit*
+fi
+
 echo "Now restart change your default shell to zsh and restart your computer!"
